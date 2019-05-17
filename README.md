d8f3a7c52e0dd2a9a2257859cc239e05fa56b225
freewind <nowindlee@gmail.com>
2019-05-17 23:49:21 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-TypeScript Node "request" Demo
+TypeScript Node "request" Send XML Demo
 ===========================
 
-使用"request"这个库来发送http请求。
+使用"request"这个库来发送xml请求。
+
+做法就是设置相应的request headers，再把XML字符串放在`body`中。
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,5 +1,13 @@
 import request, {Response} from 'request';
 
-request("https://github.com/", (error: any, response: Response, body: any) => {
+request({
+  url: "https://httpbin.org/post",
+  method: 'POST',
+  headers: {
+    Accept: 'application/xml, text/xml, */*; q=0.01',
+    'Content-Type': 'text/xml',
+  },
+  body: '<?xml version="1.0" encoding="utf-16"?><hello>request</hello>'
+}, (error: any, response: Response, body: any) => {
   console.log(body);
 })
